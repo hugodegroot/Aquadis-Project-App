5028627f89550a0dd47f3878ba4c39a3b7e60a78
Lorenzo Korn <lkorn.9520@gmail.com>
2018-11-28 18:47:22 +0100

    changes salary to number instead of string

--- src/app/racer.ts
+++ src/app/racer.ts
@@ -2,5 +2,14 @@ export class Racer {
   id: number;
   firstName: string;
   lastName: string;
-  salary: string;
+  salary: number;
+
+  /**
+   * Changes the number to a sting where the number contains thousand separators
+   *
+   * @param number salary of a racer
+   */
+  public printNumber(number: number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
 }
