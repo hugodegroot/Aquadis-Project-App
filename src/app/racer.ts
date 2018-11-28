9ce42915ade0f824637d2a8fa045c916d2faa713
Lorenzo Korn <lkorn.9520@gmail.com>
2018-11-28 19:00:16 +0100

    changed some title

--- src/app/racer.ts
+++ src/app/racer.ts
@@ -3,13 +3,4 @@ export class Racer {
   firstName: string;
   lastName: string;
   salary: number;
-
-  /**
-   * Changes the number to a sting where the number contains thousand separators
-   *
-   * @param number salary of a racer
-   */
-  public printNumber(number: number) {
-    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  }
 }
