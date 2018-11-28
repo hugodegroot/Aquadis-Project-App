1a1a2d025078964b320b293120b2d9cc32d85e64
Lorenzo Korn <lkorn.9520@gmail.com>
2018-11-28 16:33:54 +0100

    .

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -5,6 +5,7 @@ import {Injectable} from '@angular/core';
 })
 export class DataService {
 
+  // change localhost to the local ip. cmd -> ipconfig
   apiUrl: String = 'http://localhost:8080/aquadis/rest';
 
   constructor() {
