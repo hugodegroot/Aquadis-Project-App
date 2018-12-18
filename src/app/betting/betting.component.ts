45074470cc8be4a4db0eff080ed90b13e844a4be
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2018-12-18 13:31:03 +0100

    Added toggled racer class

--- src/app/betting/betting.component.ts
+++ src/app/betting/betting.component.ts
@@ -10,6 +10,7 @@ import {RacersService} from '../racers.service';
 import {Team} from '../team';
 import {TeamService} from '../team.service';
 import {GroupService} from '../group.service';
+import {Group} from '../group';
 
 @Component({
   selector: 'app-betting',
@@ -25,11 +26,15 @@ export class BettingComponent implements OnInit {
   Racers$: Racer[];
   groupID: number;
   loading: boolean = true;
+  group: Group;
 
   maxRacersErrorMessage: boolean = false;
   duplicateRacerErrorMessage: boolean = false;
+  private outOfFundsErrorMessage: boolean = false;
 
   myRacers: Racer[] = [];
+
+  private newBudget: number;
 
   constructor(private http: HttpClient,
               private userService: UserService,
@@ -44,7 +49,8 @@ export class BettingComponent implements OnInit {
         this.groupService.getUsers(this.groupID).subscribe(users => {
           this.users = users,
             this.currentUser = this.users.filter(user => user.id === this.userService.getUserId())[0],
-            console.log(this.currentUser);
+            console.log(this.currentUser),
+            groupService.getGroup(this.groupID).subscribe(group => this.group = group);
         });
     });
   }
@@ -63,35 +69,80 @@ export class BettingComponent implements OnInit {
   addRacerToPrediction(event: any, racer: Racer) {
     console.log(racer);
 
+    // @ts-ignore
+    this.newBudget = this.currentUser.groups[0].budget - racer.salary;
+
     if (this.myRacers.length < 5) {
       if (!this.myRacers.find(myRacer => myRacer == racer)) {
-        this.myRacers.push(racer);
-        this.renderer.addClass(event.target, 'toggled');
-        if (this.currentUser.groups !== null) {
-          // TODO change budget of correct group
-          this.currentUser.groups[0].budget = this.currentUser.groups[0].budget - racer.salary;
+        if (this.newBudget >= 0) {
+          this.myRacers.push(racer);
+          var elements = document.getElementsByClassName('racer_' + racer.id);
+          // for (let i = 0; i < elements.length; i++) {
+          //   this.renderer.addClass(elements[i], 'toggled');
+          // }
+          this.renderer.addClass(elements[0], 'toggled');
+          this.renderer.setStyle(elements[1], 'visibility', 'hidden');
+          this.changeBudget('-', racer.salary);
+          this.duplicateRacerErrorMessage = false;
+          this.outOfFundsErrorMessage = false;
+          this.maxRacersErrorMessage = false;
+        } else {
+          this.outOfFundsErrorMessage = true;
+          this.scrollToErrors();
         }
-        this.duplicateRacerErrorMessage = false;
       } else {
         this.duplicateRacerErrorMessage = true;
+        this.scrollToErrors();
       }
     } else {
       this.maxRacersErrorMessage = true;
+      this.scrollToErrors();
+    }
+
+    if (this.myRacers.length == 5) {
+      document.getElementById('myRacers').scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'});
     }
 
     console.log(this.myRacers);
   }
 
+  scrollToErrors() {
+    document.getElementById('racersList').scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'});
+  }
+
   removeRacerFromPrediction($event: any, racer: Racer) {
     console.log(racer);
 
-    if (this.currentUser.groups !== null) {
-      // TODO change budget of correct group
-      this.currentUser.groups[0].budget = this.currentUser.groups[0].budget + racer.salary;
-    }
+    this.changeBudget('+', racer.salary);
+
     this.myRacers.splice(this.myRacers.indexOf(racer), 1);
+
+    var elements = document.getElementsByClassName('racer_' + racer.id);
+    // for (let i = 0; i < elements.length; i++) {
+    //   this.renderer.removeClass(elements[i], 'toggled');
+    // }
+
+    this.renderer.removeClass(elements[0], 'toggled');
+    this.renderer.setStyle(elements[1], 'visibility', 'visible');
 
     this.duplicateRacerErrorMessage = false;
     this.maxRacersErrorMessage = false;
+    this.outOfFundsErrorMessage = false;
   }
+
+  changeBudget(operation: string, amount: number) {
+    // @ts-ignore
+    if (this.currentUser.groups[0].budget !== null && this.currentUser.groups[0].budget !== undefined) {
+      // TODO change budget of correct group
+      if (operation === '+') {
+        // @ts-ignore
+        this.currentUser.groups[0].budget = this.currentUser.groups[0].budget + amount;
+
+      } else if (operation === '-') {
+        // @ts-ignore
+        this.currentUser.groups[0].budget = this.currentUser.groups[0].budget - amount;
+      }
+    }
+  }
+
 }
